enmeng-liu/ICS2018

ready for ics2018

Makefile
@@ -1,0 +1,16 @@
+include nemu/Makefile.git
+
+default:
+	@echo "Please run 'make' under any subprojects to compile."
+ 
+clean:
+	-$(MAKE) -C nemu clean
+	-$(MAKE) -C nexus-am clean
+	-$(MAKE) -C nanos-lite clean
+	-$(MAKE) -C navy-apps clean
+
+submit: clean
+	git gc
+	STUID=$(STUID) STUNAME=$(STDNAME) bash -c "$$(curl -s http://moon.nju.edu.cn/people/yyjiang/teach/submit.sh)"
+
+.PHONY: default clean submit

JiaLoh/ics-2018

Makefile
@@ -1,0 +1,16 @@
+include nemu/Makefile.git
+
+default:
+	@echo "Please run 'make' under any subprojects to compile."
+ 
+clean:
+	-$(MAKE) -C nemu clean
+	-$(MAKE) -C nexus-am clean
+	-$(MAKE) -C nanos-lite clean
+	-$(MAKE) -C navy-apps clean
+
+submit: clean
+	git gc
+	STUID=$(STUID) STUNAME=$(STUNAME) bash -c "$$(curl -s http://moon.nju.edu.cn/people/yyjiang/teach/submit.sh)"
+
+.PHONY: default clean submit